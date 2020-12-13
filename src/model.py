398a5c71467efa245180e579f57fe91f1bb795ae
Matthew Mascioni <mascionim@gmail.com>
2020-12-13 18:53:56 -0500

    added submission table and submitted_by for attribution

--- src/model.py
+++ src/model.py
@@ -34,3 +34,24 @@ class Activity(db.Model):
     min_players = db.Column(db.Integer, nullable=False, default=1)
     max_players = db.Column(db.Integer, nullable=True)
     created_date = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+    submitted_by = db.Column(db.String(256), nullable=True)
+
+
+class Submission(db.Model):
+    """Represents a community submission to the game/activity index.
+    """
+
+    __tablename__ = 'submissions'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(1024), nullable=False)
+    url = db.Column(db.String(2048), nullable=False)
+    description = db.Column(TEXT, nullable=True)
+    paid = db.Column(db.Boolean, nullable=False, default=False)
+    price_desc = db.Column(db.String(100), nullable=True)
+    min_players = db.Column(db.Integer, nullable=False, default=1)
+    max_players = db.Column(db.Integer, nullable=True)
+    created_date = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+    submitted_by = db.Column(db.String(256), nullable=True)
+    approved = db.Column(db.Boolean, nullable=False, default=False)
+    archived = db.Column(db.Boolean, nullable=False, default=False)
